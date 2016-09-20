40765729d054a710147e173315f6c4dc2770a308
mensta <indextan@yahoo.com>
2016-09-20 18:31:06 +0900

    トイレの神はいない、紙はあるが。
    とりま最初の一歩っしょＪＫ

---
+++ main.bat
@@ -0,0 +1,5 @@
+:loop
+node agarcitycrazy.js
+ping 127.0.0.1 -n 4 > nul
+^C
+goto loop
---
+++ serverconfig.js
@@ -0,0 +1,4 @@
+var serverconfig = {};
+
+serverconfig.server = 'ws://149.56.131.181:24270';
+module.exports = serverconfig;
