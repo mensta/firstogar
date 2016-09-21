8bd096db651876c16445a7f30e6a2fa966d67601
mensta <indextan@yahoo.com>
2016-09-21 16:17:11 +0900

    4241
    5115

--- src/server/agarcity.js
+++ src/server/agarcity.js
@@ -1,23 +1,23 @@
 var WebSocket = require('ws');
 var fs = require('fs');
 var serverconfig = require('./serverconfig');
-var nombredelosbots = 'ttp://Fstyle.ga';
-var socks = require('socks');
+var nombredelosbots = 'TheGexiYT BOTS =)';
+var io = require('socket.io').listen(8082);
 var init1 = new Buffer(5);
 init1.writeUInt8(254, 0);
-init1.writeUInt32LE(1, 1);
+init1.writeUInt32LE(1, 1, true);
 var init2 = new Buffer(5);
 init2.writeUInt8(255, 0);
-init2.writeUInt32LE(1332175218, 1);
+init2.writeUInt32LE(1332715218, 1, true);
+var posx = 4000;
+var posy = 4000;
 var buffer2 = new Buffer(21)
-var irax = 7000;
-var iray = 7000;
-var amount = 3; //no pongas muchos o te banearan
+		buffer2.writeUInt8(16, 0);
+		buffer2.writeDoubleLE(posx, 1, true);
+		buffer2.writeDoubleLE(posy, 9, true);
+		buffer2.writeUInt32LE(0, 17, true);
+var amount = 1;
 var botcount = [];
-buffer2.writeUInt8(16, 0);
-buffer2.writeDoubleLE(irax, 1, true);
-buffer2.writeDoubleLE(iray, 9, true);
-buffer2.writeUInt32LE(0, 17, true);
 
 var paqueteparaentrar = (function conseguirelpaquete(nombre){
 	var buffer = new Buffer(1 + 2 * nombredelosbots.length);
@@ -39,11 +39,27 @@ function escribirstring8(str,L){
 return dataView;
 }
 
+
+function createAgent(ip,type) {
+
+    data = ip.split(":");
+
+    return new Socks.Agent({
+            proxy: {
+                ipaddress: data[0],
+                port: parseInt(data[1]),
+                type: parseInt(type)
+            }}
+    );
+}
+
+
+
 function crearBot(id){
-	console.log("starting // BUILDING BOTS");
+	console.log("STARTING! // BUILDING BOTS");
 	var opciones = {
 		headers:{
-			"Origin": "http://agario.city/play.php"
+			"Origin": "http://agario.city"
 		}
 	}
 	var ws = new WebSocket(serverconfig.server,null,opciones);
@@ -54,20 +70,26 @@ function crearBot(id){
 		send(init2);
 		setTimeout(function(){
 			botcount++
-			console.log('BOT_' + botcount + ' SPAWNED SUCCESS IN '+ serverconfig.server + ' BOTS TARGET: X: ' + irax + ' Y: '+ iray);
-			var split = new Buffer([17]);
-			send(split);
+
+			console.log('BOT_' + botcount + ' SPAWNED SUCCESS IN '+ serverconfig.server + ' BOTS TARGET: X: ' + "" + ' Y: '+ "");
+			//var sjoin = new Buffer([17]);
+			//send(sjoin);
 			send(paqueteparaentrar);
+			send(buffer2);
+			
+		}, 1000);
+		setTimeout(function(){
+			console.log("CLOSING SOCKET TO RESPAWN");
 			ws.close();
-      send(buffer2);
-		}, 10);
+		}, 30000);
 	});
 	ws.on('close',function(){
 		console.log('Cerrado');
-		if(continueSpam = true) setTimeout(crearBot, 10);
+		if(continueSpam = true) setTimeout(crearBot, 1000);
 	});
 	ws.on('error', function(){
-
+		console.log("ERROR CONNECTING... RECONECTING");
+		crearBot();
 	});
 	function send(mensaje){
 		try{
@@ -83,6 +105,6 @@ function crearBot(id){
     for (var i = 0; i < amount; i++) {
         setTimeout(function() {
 			crearBot(i)
-		}, (i*11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111))
+		}, (i*111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111))
 	}
-}, 10)
+}, 100)
